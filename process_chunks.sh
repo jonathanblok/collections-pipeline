f76f50a95915249cf8191c88da82f40a2a85bfe9
Blok <j.s.blok@cultureelerfgoed.nl>
2026-08-05 14:04:09 +0200

    debugging transform

--- process_chunks.sh
+++ process_chunks.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
 mv $@ data/chunk/             # Take all arguments and move them to the target directory
 sleep 1
 timestamp=$(date +%s)
-java -jar lib/shexml.jar -m config/collections_schema.shexml -f jsonld -o data/output/$timestamp.jsonld && rm data/chunk/*
+echo "processing $@ ===> chunk_$timestamp.jsonld"
+java -jar lib/shexml.jar -m config/basic_schema.shexml -f jsonld -o data/output/chunk_$timestamp.jsonld && rm data/chunk/*
