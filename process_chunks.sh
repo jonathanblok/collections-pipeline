de393c235c822151700e25dc42a7ff702df59e55
Blok <j.s.blok@cultureelerfgoed.nl>
2026-08-05 16:29:49 +0200

    added media object to basic mapping

--- process_chunks.sh
+++ process_chunks.sh
@@ -8,5 +8,5 @@ fi
 mv $@ data/chunk/             # Take file and move them to the target directory
 sleep 1
 filename=chunk_$(date +%s).jsonld
-echo "processing $@ ===> $filename"
+echo "processing $@ ===> data/output/$filename"
 java -jar lib/shexml.jar -m config/basic_schema.shexml -f jsonld -o data/output/$filename && rm data/chunk/*.xml
